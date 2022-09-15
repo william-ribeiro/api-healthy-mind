00759d77adec0e49847f78346acdb3114e1fd0dc
William Ribeiro <william_jag_rs@hotmail.com>
2022-09-15 19:57:37 -0300

    feat(resources): created resources table
    #49

---
+++ src/database/migrations/1663282373858-create_table_resources.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { DATABASE } from '../../constants';
+
+export class createTableResources1663282373858 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: DATABASE.RESOURCES,
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isGenerated: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'userId',
+            type: 'varchar',
+          },
+          {
+            name: 'category',
+            type: 'varchar',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'enabled',
+            type: 'boolean',
+            default: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_user',
+            referencedTableName: DATABASE.USERS,
+            referencedColumnNames: ['id'],
+            columnNames: ['userId'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(DATABASE.RESOURCES);
+  }
+}

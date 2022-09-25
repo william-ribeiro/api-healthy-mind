2f2240a9e894d9f16f0dc7bade10f4e4fd35412d
William Ribeiro <william_jag_rs@hotmail.com>
2022-09-25 00:11:03 -0300

    build: fixed babel config

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,9 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
+    'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
     [
       'module-resolver',
       {

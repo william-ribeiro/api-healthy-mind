26ee34df794d49ff11321e62d021e7237c2aa35a
William Ribeiro <william_jag_rs@hotmail.com>
2022-09-25 23:37:21 -0300

    fix(logs): fixed logs in auth middleware

--- src/middlewares/Authorize.ts
+++ src/middlewares/Authorize.ts
@@ -1,15 +1,23 @@
 import { ROLE_IDS } from '@/constants';
 import { AppError } from '@/errors';
 import { IResponseLocals } from '@/interfaces';
+import { logger, timeBr } from '@/shared';
 import { NextFunction, Request, Response } from 'express';
 
 function Authorize(...roles: number[]) {
   return (request: Request, response: Response<IResponseLocals>, next: NextFunction) => {
-    const { roleId } = response.locals;
+    const { roleId, id: userId } = response.locals;
 
-    if (!roles.length || !roles.includes(roleId)) throw new AppError('Unauthorized', 403);
+    try {
+      if (!roles.length || !roles.includes(roleId)) throw new AppError('Unauthorized', 403);
 
-    next();
+      next();
+    } catch (err) {
+      logger.error(
+        `${timeBr} | [UNAUTHORIZED] => [${request.method}]${request.baseUrl} | userLogged: ${userId}`,
+      );
+      throw new AppError(err.message, err.statusCode);
+    }
   };
 }
 
